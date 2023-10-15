8d7641ae3d60d12a8ae5f2aaf5d239699151bdeb
Neel317 <neeldudheliya317@gmail.com>
2023-10-14 20:38:42 -0400

    Populating database for development.

---
+++ Code/backend/populate.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const connectDB = require('./config/db');
+const Recipe = require('./models/recipe');
+const mockData = require('./output.json');
+
+
+const populating = async () => {
+  await connectDB();
+  try {
+    await Recipe.deleteMany({});
+    await Recipe.create(mockData);
+  } catch (error) {
+    console.log(error, "Something went wrong");
+  }
+}
+populating()
