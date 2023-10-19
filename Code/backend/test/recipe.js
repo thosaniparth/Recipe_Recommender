7d4169c663f2219bdbbbdfbf8155577cb95ba79c
Neel317 <neeldudheliya317@gmail.com>
2023-10-19 12:11:04 -0400

    completed most of the test cases

--- Code/backend/test/recipe.js
+++ Code/backend/test/recipe.js
@@ -28,3 +28,78 @@ describe('Post Recipes Route', () =>{
 })
 
 
+describe('Get Recipes Route', () =>{
+  describe('Get Request', () =>{
+    it("should Fetch all the documents in database", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+    it("should Fetch documents based on Ingredients", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .query({CleanedIngredients: 'salt, karela, amchur'})
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+    it("should Fetch documents based on cuisine", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .query({Cuisine: 'mexican'})
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+    it("should Fetch documents based on combination of ingredients and cuisine", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .query({CleanedIngredients: 'salt, karela, amchur', Cuisine: 'indian'})
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+    it("should Fetch documents based on type of Diet", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .query({typeOfDiet: 'Vegan'})
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+    it("should Fetch documents based on time", (done) =>{
+      chai.request(app)
+      .get('/api/v4/recipes')
+      .query({totalTime: 30})
+      .end((err, res) => {
+        if(err) {console.log("Yeh hai Error", err);}
+        res.should.have.status(200)
+        done();
+      })
+    })
+  });
+})
+
+describe('Get Cuisines Route', () =>{
+  it("should get all distinct Cuisines in database", (done) =>{
+    chai.request(app)
+    .get('/api/v4/recipes/cuisines')
+    .end((err, res) => {
+      if(err) {console.log("Yeh hai Error", err);}
+      res.should.have.status(200)
+      done();
+    })
+  })
+})
